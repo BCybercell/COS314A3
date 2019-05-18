8c90965ee18f498aab834eb15277040ca06c150b
Deane Roos <blaze.cybercell@gmail.com>
2019-05-18 22:00:50 +0200

    Added preprocessing to image Added a lot of functionality to layers Found bug in softmax for output layer (values too high), temp fix with /1000 in softmax Removed processing from node, sent to layer Added assign Random weights to utilsA

--- src/Neuron.java
+++ src/Neuron.java
@@ -5,9 +5,15 @@ public class Neuron {
     public void setInputs(double inputs){
         input =inputs;
     }
-    public void process(UtilsA utils, double [][] weights){
-//        output = utils.multiplyMatrices(input, weights, input.length, input[0].length, weights[0].length);
+//    public void process(UtilsA utils, double [][] weights){
+////        output = utils.multiplyMatrices(input, weights, input.length, input[0].length, weights[0].length);
+//    }
+
+
+    public double getInput() {
+        return input;
     }
+
     public double getOutput(UtilsA utils, double[][] w){
         return output;
     }
